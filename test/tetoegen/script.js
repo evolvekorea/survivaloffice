41d879a3c555acf8fa09a5d10fff79badee557e6
Evolve Korea <evolvekorea@gmail.com>
2025-08-10 18:32:15 +0900

    ㅇㅇ

--- test/tetoegen/script.js
+++ test/tetoegen/script.js
@@ -219,7 +219,7 @@ startBtn.addEventListener("click", () => {
     let imageUrl = "";
 
     if (selectedGender === "male") {
-      if (tetoScore <= 29) imageUrl = "https://www.survivaloffice.com/images/1.png";
+      if (tetoScore <= 29) imageUrl = "https://www.survivaloffice.com/images/egennam1.png";
       else if (tetoScore <= 49) imageUrl = "https://www.survivaloffice.com/images/2.png";
       else if (tetoScore === 50) imageUrl = "https://www.survivaloffice.com/images/3.png";
       else if (tetoScore <= 79) imageUrl = "https://www.survivaloffice.com/images/4.png";
